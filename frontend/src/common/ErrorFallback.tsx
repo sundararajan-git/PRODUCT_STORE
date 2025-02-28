7264b7a0382eba13a3c7d088087f3d2a9a4a49dd
sundararajan-git-sfhl <sundararajan.sfhl@gmail.com>
2025-02-28 22:49:34 +0530

    error boundries in frontend done

---
+++ frontend/src/common/ErrorFallback.tsx
@@ -0,0 +1,23 @@
+const ErrorFallBack = ({ error, resetErrorBoundary }: any) => {
+  console.log(error);
+  return (
+    <div className="fixed top-0 left-0 right-0 w-full h-full bg-gray-400/70 dark:bg-gray-800  flex items-center justify-center p-6 z-50">
+      <section className="w-full sm:w-2/3  md:w-1/2 lg:w-1/3 mx-auto h-fit flex flex-col p-4 bg-white dark:bg-dark rounded-xl shadow fade-up ">
+        <div className="flex flex-col gap-4 items-center justify-center p-4">
+          <h3 className="text-blue-1100 font-semibold">Error</h3>
+          <p className="dark:text-gray-500">Oops ! Something went wrong</p>
+          <div className="flex items-center gap-4">
+            <button
+              type="button"
+              className="bg-red-600 text-white px-6 py-2 rounded-lg text-sm  float-end flex items-center justify-between gap-2 font-semibold dark:font-medium"
+              onClick={resetErrorBoundary}
+            >
+              Ok
+            </button>
+          </div>
+        </div>
+      </section>
+    </div>
+  );
+};
+export default ErrorFallBack;
--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -6,15 +6,19 @@ import "./index.css";
 import { Provider } from "react-redux";
 import store from "./lib/redux/store.ts";
 import ThemeProvider from "./layouts/ThemeProvider.tsx";
+import { ErrorBoundary } from "react-error-boundary";
+import ErrorFallBack from "./common/ErrorFallBack.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <ThemeProvider>
-      <Provider store={store}>
-        <BrowserRouter>
-          <App />
-        </BrowserRouter>
-      </Provider>
-    </ThemeProvider>
+    <ErrorBoundary FallbackComponent={ErrorFallBack}>
+      <ThemeProvider>
+        <Provider store={store}>
+          <BrowserRouter>
+            <App />
+          </BrowserRouter>
+        </Provider>
+      </ThemeProvider>
+    </ErrorBoundary>
   </StrictMode>
 );
